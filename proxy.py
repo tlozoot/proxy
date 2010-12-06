0ba68e835bcdc03ab009696ce75a41b463e4adcd
Jon Levine <tlozoot@gmail.com>
2010-12-06 00:56:59 -0500

    get ready for some html parsing

--- proxy.py
+++ proxy.py
@@ -1,24 +1,21 @@
 import urllib
-from HTMLParser import HTMLParser
-
-class MyHTMLParser(HTMLParser):
-
-    def handle_starttag(self, tag, attrs):
-        print "Encountered the beginning of a %s tag" % tag
-
-    def handle_endtag(self, tag):
-        print "Encountered the end of a %s tag" % tag
 
 from flask import Flask, request
 app = Flask(__name__)
 
-
 @app.route("/pass")
 def proxy_pass():
+    from custom_parser import AssetAttributeFixer
+
     try:
-        return urllib.urlopen(request.args.get("url")).read()
+        url_sock = urllib.urlopen(request.args.get("url"))
+        text = url_sock.read()
+        url_sock.close()
     except:
         return "Please include a valid URL parameter" 
+        
+    parser = AssetAttributeFixer()
+    return text
 
 if __name__ == "__main__":
     app.run(debug=True)
